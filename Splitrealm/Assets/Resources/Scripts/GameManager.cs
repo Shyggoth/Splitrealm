99e43e1e7a16489741d1ab9b39644480924b9459
Gyosei <shyggoth@gmail.com>
2020-11-17 00:23:32 +0100

    asdg

---
+++ Splitrealm/Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour, IEnumerable
+{
+	public List<PlayerContainer> listOfPlayers = new List<PlayerContainer>();
+	public Tilemap tileMap;
+
+	public IEnumerator GetEnumerator()
+	{
+		throw new System.NotImplementedException();
+	}
+}
